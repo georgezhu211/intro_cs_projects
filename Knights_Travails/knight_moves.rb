4f37154d08b1106e67dcbd34ef199c6704144184
George <georgezhu211@gmail.com>
2020-11-29 23:40:46 -0600

    add Chess and Knight class

--- Knights_Travails/knight_moves.rb
+++ Knights_Travails/knight_moves.rb
@@ -1,3 +1,33 @@
+class Chess
+  def initialize
+    @board = Board.new
+    @knight = Knight.new(@board)
+  end
+
+  def knight_moves(pos)
+    place_piece(pos, @knight.mark)
+    display_board
+  end
+
+  def place_piece(pos, mark)
+    @board.place_piece(pos, mark)
+  end
+
+  def display_board
+    @board.display
+  end
+end
+
+class Knight
+  attr_reader :mark
+  def initialize(board)
+    @board = board
+    @mark = 'K'
+  end
+
+
+end
+
 class Board
   def initialize
     @board = Array.new(8) { Array.new(8, ' ') }
@@ -30,7 +60,12 @@ class Board
     tmp.push("|")
     tmp.join
   end
+
+  def place_piece(pos, mark)
+    x, y = pos[0], pos[1]
+    @board[x][y] = mark
+  end
 end
 
-board = Board.new
-board.display
+chess = Chess.new
+chess.knight_moves([4,4])
